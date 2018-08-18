092b7fc8e01c5f0683454661607d6cd2c6462dc1
Apoorva Teli <apoorvateli@gmail.com>
2018-08-18 16:53:53 +0530

    Add more comments to improve explanation

--- IsPrime.java
+++ IsPrime.java
@@ -4,7 +4,9 @@ public class IsPrime {
   public static boolean isPrime(int n) {
     // A number n is a prime number if it is divisible only by 1 and itself(n) 
     // and not any other number between 1 to n
+    // i.e. If 1 and n are the only two factors of n, then n is prime.
     int count = 0; // count the numbers that n is divisibly by, form 2 to n-1
+    // If there are no factors of n between 2 and n-1, then n is prime
 
     for (int i = 2; i < n; i++) {
       if (n%i == 0)
