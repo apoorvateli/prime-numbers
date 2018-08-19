2a3a6c63d90241fa83133dae5dc14ab33623cd75
Apoorva Teli <apoorvateli@gmail.com>
2018-08-19 21:30:11 +0530

    In IsPrimeUsingRecursion program, optimize i to go from 2 to sqrt(n) instead of n/2 to 2

--- IsPrimeUsingRecursion.java
+++ IsPrimeUsingRecursion.java
@@ -8,10 +8,10 @@ public class IsPrimeUsingRecursion {
     System.out.print("Enter a number: ");
     int n = sc.nextInt();
 
-    i = n/2;
-    // i will go from n/2 to 1
-    // we will check if any number(i) from n/2 to 2 is a factor of n, then isPrime will return false
-    // else when i reaches 1, no number up to 2 was a factor of n, therefore isPrime returns true
+    i = 2;
+    // i will go from 2 to sqrt(n)
+    // we will check if any number(i) from 2 to sqrt(n) is a factor of n. If yes, then isPrime will return false
+    // else when i reaches sqrt(n), if no number was a factor of n, isPrime will true
 
     if (isPrime(n)) 
       System.out.println(n+" is a prime number");
@@ -19,12 +19,17 @@ public class IsPrimeUsingRecursion {
   }
 
   public static boolean isPrime(int n) {
-    if (n<=1) return false;
+    // Base cases
+    if (n<=1) 
+      return false;
 
-    if (i==1) return true;
-    else if (n%i == 0) return false;
+    if (i > Math.sqrt(n)) return true; // stop when i is just greater than sqrt(n). No factor was found
+    
+    if (n%i == 0) return false; // if n is divisible by any no. from 2 to sqrt(n), n is not prime
+
+    // check for next divisor
     else {
-      i = i-1;
+      i = i+1;
       return isPrime(n);
     }
   }
